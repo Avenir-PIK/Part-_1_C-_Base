1de32cb230a552681807c3511ed417907bc8658a
Avenir Baryshev <avenirpik@gmail.com>
2025-02-19 16:57:04 +0700

    upd 2_4

--- Homework_2_4_digitSwap/Program.cs
+++ Homework_2_4_digitSwap/Program.cs
@@ -22,17 +22,27 @@ namespace Homework_2_4_digitSwap
             //почему то не даёт это сделать шортом
             int newdigit = (d1 * 1000) + (d4 * 100) + (d3 * 10) + (d2);
 
+            //проверка замены через строку
+            string testik1 = Convert.ToString(d1) + Convert.ToString(d4) + Convert.ToString(d3) + Convert.ToString(d2);
+            short testik2 = Convert.ToInt16(testik1);
+
             // меняем цифры местами на выводе, выводим набор цифр
 
-            Console.WriteLine("Вывод как набор цифр");
+            Console.WriteLine("Вывод как набор цифр byte");
             Console.WriteLine("Было {0}, а стало {1}{2}{3}{4}", digit, d1, d4, d3, d2);
 
             Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
             // вывод как число из цифр
 
-            Console.WriteLine("Вывод как единое число");
+            Console.WriteLine("Вывод как единое число через int");
             Console.WriteLine("Было {0}, а стало {1}", digit, newdigit);
 
+            Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+
+            // вывод шорта с заменой символов через строку
+
+            Console.WriteLine("Вывод шорта с заменой символов через string");
+            Console.WriteLine("Было {0}, а стало {1}", digit, testik2);
 
             Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
             Console.WriteLine("Find and press ANY key to continue");
