10d21ce7bb1b7d26e0bf9b144eb55b4f6fc59d7d
Avenir Baryshev <avenirpik@gmail.com>
2025-02-19 17:18:07 +0700

    2_6+

--- Homework_2_6_round_0,5/Program.cs
+++ Homework_2_6_round_0,5/Program.cs
@@ -9,18 +9,13 @@ namespace Homework_2_4_digitSwap
             Console.WriteLine("Введите длину трубы:");
 
             double iPipeLenght = Convert.ToDouble(Console.ReadLine());
-            
-            // Лучше приводить через явное преобразование или через конверт?
 
-            // через конверт
-            //int oPipeLenght = Convert.ToInt32(Math.Ceiling(iPipeLenght));
-            
-            // через явное
-            int oPipeLenght = (int)(Math.Ceiling(iPipeLenght));
+            // умножаем на 2, чтобы округлять до ближайшей 1цы, после чего снова делим на 2
 
+            double oPipeLenght = Math.Round(iPipeLenght*2)/2;
 
             Console.WriteLine("");
-            Console.WriteLine("Длина трубы округлена вверх");
+            Console.WriteLine("Длина трубы округлена до ближайших 0,5");
             Console.WriteLine("");
             Console.WriteLine("Было {0}, а стало {1}", iPipeLenght, oPipeLenght);
             Console.WriteLine("");
