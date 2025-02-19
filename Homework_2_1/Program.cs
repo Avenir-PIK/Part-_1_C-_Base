76112f4c1893df903b94c9269e35a735bb957a21
Avenir Baryshev <avenirpik@gmail.com>
2025-02-19 15:28:48 +0700

    ready

--- Homework_2_1/Program.cs
+++ Homework_2_1/Program.cs
@@ -4,8 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            //test
+            Console.WriteLine("Введите трехзначное число");
+            int dig = Convert.ToInt32(Console.ReadLine());
+
+            int d1 = dig / 100;
+            int d2 = (dig / 10) % 10;
+            int d3 = dig % 10;
+
+            Console.WriteLine("{2}{0}{1}", d1, d2, d3);
+
+            Console.WriteLine("Find and press ANY key to continue");
+            Console.ReadKey();
         }
     }
 }
